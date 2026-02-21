c035a77627fbcf6fb0f5ec1f1b0a140c0fed2411
Jake Gendreau <jake.lgendreau@gmail.com>
2026-02-21 15:39:44 -0800

    Converted HTML to Jinja templates and made it work with flask

--- Website/app.py
+++ Website/app.py
@@ -2,12 +2,13 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        name = request.form['username']
-        return f"Hello {name}, POST request received"
-    return render_template('name.html')
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/report', methods=['GET'])
+def report():
+    return render_template('report.html')
 
 if __name__ == '__main__':
     app.run()
